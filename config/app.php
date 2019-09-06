3c29a63334d7a76fae12c6f9006e416b553b1aa1
Anuj Jaha <er.anujjaha@gmail.com>
2019-09-06 10:55:36 +0530

    added PT Mirani as Title

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Patel Hospital'),
+    'name' => env('APP_NAME', 'P T Mirani Hospital'),
 
     /*
     |--------------------------------------------------------------------------
